50eaf3c3fd2fa12d2efd14833101c1bde5243b93
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-25 17:40:16 +0200

    "creatin start server function and calling inside the start function"

---
+++ notification-service/src/server.ts
@@ -0,0 +1,32 @@
+import 'express-async-errors';
+import { Logger } from 'winston';
+import { config } from '@notifications/config';
+import { winstonLogger } from '@quickstartfreelancing/common';
+import { Application } from 'express';
+import http from 'http';
+
+const SERVER_PORT = 4001;
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'notificationServer', 'debug');
+export function start(app:Application): void {
+    startServer(app);
+}
+
+async function startQueue(): Promise<void>{
+
+}
+
+function startElasticSearch(): void {
+
+}
+
+function startServer(app:Application): void {
+    try {
+        const httpServer: http.Server = new http.Server(app);
+        log.info(`Worker with process id of ${process.pid} on notification server has started`);
+        httpServer.listen(SERVER_PORT, () => {
+            log.info(`Notification server is listening on port ${SERVER_PORT}`);
+        });
+    } catch (error) {
+        log.log('error','NotificationService startServer() method:',error);
+    }
+}
