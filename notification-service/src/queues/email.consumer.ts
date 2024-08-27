7cf821549c87d6c8099395bd2768882fcda7148b
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-27 15:31:00 +0200

    "creating consume auth email messages fuction"

---
+++ notification-service/src/queues/email.consumer.ts
@@ -0,0 +1,31 @@
+import { Channel, ConsumeMessage } from 'amqplib';
+import { winstonLogger } from '@quickstartfreelancing/common';
+import { config } from '@notifications/config';
+import { Logger } from 'winston';
+import { createConnection } from './connection';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'notificationQueueConnection', 'debug');
+
+async function consumeAuthEmailMessages(channel: Channel): Promise<void> {
+    try {
+        if (!channel){
+            channel = await createConnection() as Channel;
+        }
+
+        const exchangeName = 'quickstart-email-notification';
+        const routingKey = 'auth-email';
+        const queueName = 'auth-email-queue';
+
+        await channel.assertExchange(exchangeName, 'direct');
+        const quickStartQueue = await channel.assertQueue(queueName, { durable: true, autoDelete: false });
+        await channel.bindQueue(quickStartQueue.queue, exchangeName, routingKey);
+
+        channel.consume(quickStartQueue.queue, async (msg: ConsumeMessage | null) => {
+            console.log(JSON.parse(msg!.content.toString()));
+        });
+    } catch (error) {
+        log.log('error', 'Notification Serive EmailConsumer consumeAuthEmailMessages() method error:' ,error);
+    }
+}
+
+export { consumeAuthEmailMessages };
